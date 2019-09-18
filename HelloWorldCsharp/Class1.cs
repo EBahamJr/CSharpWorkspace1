af06d060f8af962615570a3b96e4ad8744501669
Eric Baham Jr <ejbaham01@gmail.com>
2019-09-18 15:36:09 -0400

    initial commit

---
+++ HelloWorldCsharp/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCsharp
+{
+    class Class1
+    {
+        int[] a;
+
+        public Class1(int num, int multiples)
+        {
+            this.a = new int[multiples];
+            filler(num, a);
+        }
+
+        private void filler(int n, int[] a)
+        {
+            for (int i = 0; i < a.Length; i++ )
+            {
+                this.a[i] = n * (i+1);
+            }
+        }
+
+        public override String ToString()
+        {
+            var line = "";
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                line = line + a[i] + " ";
+            }
+
+            return line;
+        }
+
+    }
+}
---
+++ HelloWorldCsharp/TacoOB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCsharp
+{
+    class TacoOB
+    {
+        private Boolean cheese;
+        private String meat;
+        private Boolean lettuce;
+        private 
+
+        
+    }
+}

1642a925d631d2b4b22d3070cbbc2c650b6dcd65
EBahamJr <ejbaham01@gmail.com>
2019-09-25 16:09:35 -0400

    Fixed Previous commit

---
+++ TestApi.Contracts/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApi.Contracts.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
